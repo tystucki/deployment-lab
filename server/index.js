045c418078e05537664c5cdd0085412189596e4b
Ty Stucki <tystucki@Tys-MacBook-Pro.local>
2022-09-21 13:57:57 -0700

    trying to fix bugs

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.js'))
+    res.sendFile(path.join(__dirname, '../client/styles.css'))
 })
 
 app.listen(port, () => {
